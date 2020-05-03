505897c9a6892dc1ae9a42e8ff6978478556847d
Ryan Achten <ryan.achten@akqa.com>
2020-05-03 15:01:15 +1200

    Add init signalr hub for streaming

---
+++ api/Hubs/StreamHub.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+namespace api.Hubs
+{
+    public class StreamHub : Hub
+    {
+
+    }
+}
--- api/Startup.cs
+++ api/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace Echo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +47,7 @@ namespace Echo
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<StreamHub>("/stream");
             });
         }
     }
