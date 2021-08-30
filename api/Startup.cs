78fb0f26c907d2e9ee711657aeff24b99b562c44
RyanAchten <ryanachten@gmail.com>
2021-08-30 16:47:25 +1200

    Move CORS origin into appsettings

--- api/Startup.cs
+++ api/Startup.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using api.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -20,8 +21,8 @@ namespace Echo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var allowedOrigins = Environment.GetEnvironmentVariable("ECHO_ALLOWED_ORGINS");
-
+            var origins = Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>();
+            var test = string.Join(',', origins);
             services.AddControllers();
             services.AddSignalR();
 
@@ -31,7 +32,7 @@ namespace Echo
                 {
                     policy.AllowAnyHeader()
                         .AllowAnyMethod()
-                        .WithOrigins(allowedOrigins)
+                        .WithOrigins(origins)
                         .AllowCredentials();
                 });
             });
